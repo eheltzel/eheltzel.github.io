eeac792f58fb445f63f78df6abc4ab8dd96b5b10
eheltzel <emilyh555@gmail.com>
2022-10-06 16:33:07 -0400

    Automated table creation for webmap reviews.

--- javascript/external.js
+++ javascript/external.js
@@ -12,8 +12,14 @@ function welcome()
   return message;
 }
 
-// document.write(welcome());
-
-// A prompt box is used to prompt users to input a value before entering a page.
-// user_name = window.prompt("Please enter your name", "Type your name here");
-// document.write(`<h1>Hello, Welcome to my webpage, ${user_name}!</h1>`);
+function webmap_table(){
+  document.write("<table width=100%>")
+  for(var row = 0; row <  2; row++){
+    document.write("<tr>");
+    for(var column = 0; column < 3; column++){
+      document.write("<td>"+row+","+column+"</td>");
+    }
+    document.write("</tr>")
+  }
+  document.write("</table>")
+}
