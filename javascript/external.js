9eba80df4aed79c228cee66834007569d8c7c99a
eheltzel <emilyh555@gmail.com>
2022-10-06 16:49:50 -0400

    Filled content from web maps array into the web maps table.

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,18 @@
 
+var webmaps =
+[
+    ["MassMapper",
+    "https://maps.massgis.digital.mass.gov/MassMapper/MassMapper.html",
+    "The primary purpose of MassMapper is to provide an online, interactive map of Massachusetts with several reference layers provided. These layers can be used for a variety of purposes and allow for anyone to make their own map. The user can choose from hundreds of layers from within the MassGIS database and add them to the map."
+    ],
+
+    ["PolicyMap",
+    "https://www.policymap.com/newmaps#/",
+    "The primary purpose of PolicyMap is to bridge the gap between data and action. The application allows the user to choose from several different categories such as demographics, education, housing, quality of life, health, and more. It allows for people to make detailed maps with no training or GIS experience."
+    ]
+];
+
+
 function welcome()
 {
   let a = "Please enter your name.";
@@ -14,10 +28,10 @@ function welcome()
 
 function webmap_table(){
   document.write("<table width=100%>")
-  for(var row = 0; row <  2; row++){
+  for(var row = 0; row <  webmaps.length; row++){
     document.write("<tr>");
-    for(var column = 0; column < 3; column++){
-      document.write("<td>"+row+","+column+"</td>");
+      for(var column = 0; column < webmaps[0].length; column++){
+        document.write("<td>"+webmaps[row][column]+"</td>");
     }
     document.write("</tr>")
   }
